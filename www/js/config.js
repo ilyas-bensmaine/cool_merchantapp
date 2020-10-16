cf4f11403224ec036c52875719417561876cc3b4
ilyasBensmaine <istonya.16@gmail.com>
2020-10-16 17:52:33 +0100

    debug mode config

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var merchantapp_config ={							
-	'ApiUrl':"",
-    'AppTitle':"KARENDERIA",
-	'ApiKey' : '',	
-	'debug': false
+	'ApiUrl':"https://cool724.com/merchantappv2/api",
+    'AppTitle':"Cool724 Merchant App",
+	'ApiKey' : '804eb0cbc849f21c4fb54fe15a27a4a2',	
+	'debug': true
 };
